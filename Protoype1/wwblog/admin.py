aa4e7c2afab97932aab231c402399b734174bd34
SbrowneA <samuel.browne.mx@gmail.com>
2021-01-19 12:56:53 +1030

    Remake app - delete

--- Protoype1/wwblog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Article\
-    # , ArticleEditor
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Article)
-# admin.site.register(ArticleEditor)
--- wwblog/wwapp/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-from .model_tests import *
-from .handler_tests import *
