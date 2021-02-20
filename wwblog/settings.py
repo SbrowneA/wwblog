ff8d1ee4e744f4d43c39d80afa2c995a18eb53d8
SbrowneA <samuel.browne.mx@gmail.com>
2021-02-20 22:54:36 +1030

    adding actual ALLOWED_HOSTS domain

--- wwblog/settings.py
+++ wwblog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['WWBLOG_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["wwblog-test2.herokuapp"]
 
 # Application definition
 
