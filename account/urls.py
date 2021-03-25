06f5995907765780b85f4da85ee4a48e1b4c6d54
SbrowneA <Samuel.browne.mx@gmail.com>
2021-03-25 22:30:13 +1030

    Added Reset Password Functionality

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 # from django.conf.urls.static import static
 # from django.conf import settings
@@ -10,12 +11,33 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
-    # path('reset-password/<str:key>/', views.reset_password, name='register'),
-    # path('reset-password/recovery-email/', views.recovery_email, name='register'),
-    path('change-password/', views.change_password, name='change_password'),
     path('register/success/', views.register_success, name='register_success'),
     path('unverified/', views.unverified_user, name='unverified_user'),
+
+    path('change-password/', views.change_password, name='change_password'),
+
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(email_template_name="account/password_reset_email.html",
+                                              subject_template_name="account/password_reset_subject.txt",
+                                              template_name="account/reset_send_email.html",
+                                              success_url="sent/"),
+         name='password_reset'),
+
+    path('password_reset/sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name="account/reset_email_sent.html"),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(success_url="/reset-password/success",
+                                                     template_name="account/reset_password.html"),
+         name='password_reset_confirm'),
+
+    path('reset-password/success',
+         auth_views.PasswordResetCompleteView.as_view( template_name="account/reset_password_success.html"),
+         name='password_reset_complete'),
 ]
+
+# reset password urls are linked statically in templates (can't be referenced dynamically)
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
