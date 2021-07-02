731b1d96916c360122877d56142a034806565221
SbrowneA <samuel.browne.mx@gmail.com>
2021-07-02 21:02:29 +0930

    UI improvements and additions
    - removed generic popupContainer: it is easier to give each popup its own mask/container
    - renamed dropzone_custom.js -> edit_article.js: to contain more relevant functionality for the edit_article.html page.
    - changed edit navigation buttons to have dynamic urls via their value property.
    - added getCookie() method to access csrf cookie and others

---
+++ static/wwapp/js/edit_article.js
@@ -0,0 +1,259 @@
+Dropzone.autoDiscover = false;
+
+let imgDz;
+
+const imageDZPopupContainerId = "imageDZPopupContainer"
+
+/**
+ * creates a hidden input field with the csrf token for forms
+ * @returns {HTMLInputElement}
+ */
+function getCSRFToken() {
+    let token = document.createElement("input");
+    token.type = 'hidden'
+    token.name = 'csrfmiddlewaretoken'
+    token.value = getCookie('csrftoken');
+    return token;
+}
+
+
+function initImageDz() {
+    console.log("init called");
+    // create and append elements to popupContainer programmatically
+    //container div
+    let container = document.createElement("div");
+    container.classList.add("popup-container");
+    container.classList.add("hide-popup");
+    container.id = imageDZPopupContainerId;
+
+    //parent div
+    let popup = document.createElement("div");
+    // let popup = document.getElementById('genericPopup');
+    popup.id = 'imageDzPopup';
+    popup.classList.add('popup');
+    // dz form within parent div
+    let form = document.createElement("form");
+    // let form = document.getElementById("genericPopupForm");
+    form.id = 'image-dz';
+    form.method = 'POST';
+    // form.action = '';
+    form.classList.add('dropzone');
+    form.classList.add('dz-custom');
+    form.appendChild(getCSRFToken());
+    //fallback div within form
+    // let fallback = document.createElement("div");
+    // input within fallback
+
+    //close button within parent div
+    let closeBtn = document.createElement("button");
+    closeBtn.id = 'dzCloseBtn';
+    closeBtn.innerText = 'close';
+    popup.appendChild(form)
+    popup.appendChild(closeBtn)
+    container.appendChild(popup);
+    document.body.appendChild(container);
+
+    // initialise dropzone form
+    if (document.getElementById('image-dz')) {
+        imgDz = new Dropzone("#image-dz",
+            {
+                url: "/upload-to-imgur",
+                timeout: 50000,
+                maxFiles: 4,
+                maxFilesize: 20,
+                // acceptedFiles: '.png, .jpg, .gif, .svg, .webp',
+                acceptedFiles: '.png, .jpg, .jpeg, .gif, .svg, .webp, .apng, .TIFF, .MP4, .MPEG, .AVI, .WEBM, .quicktime, .x-matroska, .x-flv, .x-msvideo, .x-ms-wm',
+                dictDefaultMessage: "Drop images or click here to upload<br/><span>Images are automatically be added to your images and embedded in this post</span>",
+                // addRemoveLinks: true,
+                // dictRemoveFile: "Delete",
+                // dictRemoveFileConfirmation: "Are you sure you want to delete this image?",
+            });
+
+        imgDz.on("success", (file, response) => {
+            response = JSON.parse(response)
+            let ratio = response['height'] / response['width'];
+            let newWidth = 400;
+            let newHeight = Math.floor(newWidth * ratio);
+            console.log(`response: ${response} \n new dimensions:${newWidth} x ${newHeight}`);
+            tinyMCE.activeEditor.setContent(`${tinyMCE.activeEditor.getContent()}<p><img src="${response['url']}" alt="image upload ${file.name}" width="${newWidth}" height="${newHeight}" /></p>`);
+        });
+    }
+    console.log("init end");
+}
+
+
+/**
+ * Keeps track of the container elements that have had an onclick set via the togglePopupContainer() function
+ * @type {{}}
+ */
+let containerOnClickSet = {};
+
+/**
+ * Toggles the popup container that masks the whole screen to be hidden or show with the .hide-popup class
+ * @param elementID the id of the container to be toggled
+ * @param closeFunc the function that should be called when the container is clicked to hide the popup again
+ */
+function togglePopupContainer(elementID, closeFunc) {
+    console.log("togglePopupContainer");
+    let popupContainer = document.getElementById(elementID);
+    popupContainer.classList.toggle("hide-popup");
+    if (!containerOnClickSet[elementID]) {
+        console.log("togglePopupContainer - onclick");
+        popupContainer.addEventListener("click", (e) => {
+            console.log("container clicked: " + e.target.id);
+            if (elementID === e.target.id) {
+                closeFunc();
+            }
+        });
+        containerOnClickSet[elementID] = true;
+        console.log("Container Onclick Set");
+    }
+    console.log("togglePopupContainer - end");
+    console.log(containerOnClickSet);
+}
+
+// upload image DZ functions
+/**
+ * Toggles the imageDzPopup and the popupContainer
+ */
+function toggleDzPopup() {
+    togglePopupContainer(imageDZPopupContainerId, closeDzPopup);
+    console.log("toggleDzPopup");
+}
+
+/**
+ * called to by the uploadImageBtn in the edit article page
+ * @returns {Promise<void>}
+ */
+async function openImageDzPopup() {
+    if (imgDz === undefined) {
+        initImageDz();
+        console.log("initDz() called");
+        let closeBtn = document.getElementById('dzCloseBtn');
+        closeBtn.addEventListener("click", closeDzPopup);
+    }
+    toggleDzPopup();
+}
+
+function closeDzPopup() {
+    toggleDzPopup();
+    console.log("closeDzPopup()");
+}
+
+let uploadImageBtn = document.getElementById('uploadImageBtn');
+uploadImageBtn.addEventListener("click", () => {
+    openImageDzPopup();
+});
+
+
+//Publish popup code
+const publishPopupContainerId = "publishPopupContainer";
+const closePublishBtnId = "closePublishBtn";
+let publishPopup = undefined;
+
+async function initPublishPopup() {
+    //add container
+    let container = document.createElement("div");
+    container.id = publishPopupContainerId;
+    container.classList.add("popup-container");
+    container.classList.add("hide-popup");
+
+    //add popup
+    let popup = document.createElement("div");
+    popup.classList.add("popup");
+    let closeBtn = document.createElement("button");
+    closeBtn.innerText = "close";
+    closeBtn.id = closePublishBtnId;
+    closeBtn.addEventListener("click", closePublishPopup);
+    let publishBtn = document.createElement("button");
+    publishBtn.innerText = "publish";
+    publishBtn.disabled = true;
+    publishBtn.id = closePublishBtnId;
+    publishBtn.addEventListener("click", publishArticle);
+    publishPopup = popup;
+    //TODO add create buttons
+    // ajax to load options getPublishOptions().then()
+    // add creatButton for each project loaded
+        // set id value of create btn to id of category
+        // e.g. id:"btn-create-{id}" value:{id}
+        // onclick (e) => create child cat with e.value
+
+    //add form
+    let form = document.createElement("form");
+
+    //add csrf token
+    getCSRFToken();
+
+    //append elements
+    document.body.appendChild(container);
+    container.appendChild(popup);
+    popup.appendChild(form);
+    popup.appendChild(closeBtn);
+    popup.appendChild(publishBtn);
+
+}
+
+async function openPublishPopup() {
+    if (publishPopup === undefined) {
+        initPublishPopup();
+    }
+    togglePublishPopup();
+}
+
+function closePublishPopup() {
+    togglePublishPopup();
+}
+
+function togglePublishPopup() {
+    // let pubPopup = document.getElementById(publishPopupContainerId);
+    // pubPopup.classList.toggle("hide-popup");
+    togglePopupContainer(publishPopupContainerId, closePublishPopup);
+}
+
+let publishBtn = document.getElementById("publishPopupBtn");
+publishBtn.addEventListener("click", () => {
+    openPublishPopup();
+});
+
+
+// ajax methods
+async function publishArticle(articleId){}
+async function createCategory(categoryName){}
+async function getPublishOptions(){}
+
+
+
+//NAVIGATION BUTTONS
+function viewArticle(url) {
+    console.log(url);
+    if (confirm("All unsaved changes will be lost.\nwould yo like to continue?")) {
+        //TODO do ajax
+        window.location.replace(url);
+    } else {
+        return false;
+    }
+}
+
+/**
+ * gets the url from the value of the click event (i.e. the button.value) and forwards it to {@link viewArticle()}
+ */
+document.getElementById("viewArticleBtn").addEventListener("click", (e) => {
+    viewArticle(e.target.value);
+});
+
+function deleteArticle(url) {
+    if (confirm("Are you sure you want to delete this post (" + '{{ article.article_title }}'
+        + ")?\nLinked posts will not be deleted, only unliked")) {
+        //TODO do ajax
+        window.location.replace(url);
+    } else {
+        return false;
+    }
+}
+
+/**
+ * gets the url from the value of the click event (i.e. the button.value) and forwards it to {@link deleteArticle()}
+ */
+document.getElementById("deleteArticleBtn").addEventListener("click", (e) => {
+    deleteArticle(e.target.value);
+});
--- static/wwapp/js/main.js
+++ static/wwapp/js/main.js
@@ -1,29 +1,3 @@
-function linked() {
-    alert("JS linked");
-}
-
-//
-// function deleteArticle(the_id) {
-//     alert("THe id: " + the_id.toString());
-//     if (confirm("Are you sure you want to delete this post (" + '{{ article.article_title }}'
-//         + ")?\nLinked posts will not be deleted, only unliked")) {
-//         var url = "/post/" + the_id.toString() + "/delete";
-//         alert(url);
-//         window.location.replace(url)
-//     } else {
-//         return false;
-//     }
-// }
-
-// function deleteArticle(article_id) {
-//     alert("clickd\n id:" + article_id)
-//     if (confirm("Are you sure you want to delete this post?\n linked posts will not be deleted, only unliked")) {
-//         window.location.replace("post/" + article_id + "/delete");
-//     } else {
-//         return false;
-//     }
-// }
-
 
 function convert_date_to_user_timezone() {
     const month = {
@@ -43,7 +17,7 @@ function convert_date_to_user_timezone() {
     let dateTxt = document.getElementById("publish-date");
     let dateStr = dateTxt.innerText
     let date = new Date(dateStr.toString());
-    let localDate = new Date(date.getTime() - date.getTimezoneOffset()*60000);
+    let localDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000);
     // alert(`${month[localDate.getMonth()]} ${localDate.getDate()}, ${localDate.getFullYear()}, ${localDate.getHours()}:${localDate.getMinutes()}`)
     let hr = localDate.getHours();
     let min = localDate.getMinutes();
@@ -52,6 +26,27 @@ function convert_date_to_user_timezone() {
         hr -= 12;
     }
     hr = (hr === 0) ? 12 : hr;
-    min = (min<10) ? "0"+min : min;
+    min = (min < 10) ? "0" + min : min;
     dateTxt.innerText = `${month[localDate.getMonth()]} ${localDate.getDate()}, ${localDate.getFullYear()}, ${hr}:${min} ${ampm}`;
+}
+
+/**
+ * searches the document.cookie object to get the value of the cookie name specified if it exists
+ * @param name the name of the cookie to get the value for
+ * @returns {null} the value of the cookie name if it exists
+ */
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Check the cookie string begins with name
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
